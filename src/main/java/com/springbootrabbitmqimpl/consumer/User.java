c9419fa114b5947446e9d9f3c83ad69dab432314
Ernest <inyange8888@gmail.com>
2023-05-14 20:51:54 +0100

    fixes

--- src/main/java/com/springbootrabbitmqimpl/consumer/User.java
+++ src/main/java/com/springbootrabbitmqimpl/consumer/User.java
@@ -12,6 +12,6 @@ public class User {
 
     @RabbitListener(queues = QUEUE)
     public void consumeMessageFromQueue(OrderStatus orderStatus) {
-        log.info("Message received from queue : {}", orderStatus);
+//        log.info("Message received from queue : {}", orderStatus);
     }
 }
