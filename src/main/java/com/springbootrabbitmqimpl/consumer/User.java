ee1730a8aa1acea972d6bae6cc718221065a7a40
Ernest <inyange8888@gmail.com>
2023-05-15 16:31:18 +0100

    fixes

--- src/main/java/com/springbootrabbitmqimpl/consumer/User.java
+++ src/main/java/com/springbootrabbitmqimpl/consumer/User.java
@@ -4,6 +4,7 @@ import com.springbootrabbitmqimpl.dto.OrderStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
 import static com.springbootrabbitmqimpl.Constants.*;
 
 @Slf4j
@@ -12,6 +13,6 @@ public class User {
 
     @RabbitListener(queues = QUEUE)
     public void consumeMessageFromQueue(OrderStatus orderStatus) {
-//        log.info("Message received from queue : {}", orderStatus);
+        log.info("Message received from queue : {}", orderStatus);
     }
 }
